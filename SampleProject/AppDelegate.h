5cd67c894a5f21d23450397f5ffdedaf72b61e39
singhyadav <Manu@manohars-mac-mini.local>
2016-07-18 14:43:13 -0700

    Initial Commit

---
+++ SampleProject/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  SampleProject
+//
+//  Created by Manohar T on 18/07/16.
+//  Copyright © 2016 Manohar T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ SampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleProject
+//
+//  Created by Manohar T on 18/07/16.
+//  Copyright © 2016 Manohar T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
